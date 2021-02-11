486caf708287b1b24a858f40f341f71a824180c5
NR598 <norm.rich@gmail.com>
2021-02-11 03:40:37 -0500

    update model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :loads
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
+  end
 end
